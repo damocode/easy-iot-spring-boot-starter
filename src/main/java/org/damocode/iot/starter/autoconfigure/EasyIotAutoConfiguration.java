9314c9a48d539c68ad3c043a929e2eb882c9d3c1
张争光 <skyuck@gmail.com>
2021-12-04 15:57:13 +0800

    '同步gitee代码'

--- src/main/java/org/damocode/iot/starter/autoconfigure/EasyIotAutoConfiguration.java
+++ src/main/java/org/damocode/iot/starter/autoconfigure/EasyIotAutoConfiguration.java
@@ -7,8 +7,14 @@ import org.damocode.iot.core.device.DeviceOperationBroker;
 import org.damocode.iot.core.device.DeviceOperatorManager;
 import org.damocode.iot.core.device.IDeviceOperatorService;
 import org.damocode.iot.core.device.StandaloneDeviceMessageBroker;
+import org.damocode.iot.core.server.DecodedClientMessageHandler;
 import org.damocode.iot.core.server.MessageHandler;
 import org.damocode.iot.core.server.session.DeviceSessionManager;
+import org.damocode.iot.device.message.DeviceMessageConnector;
+import org.damocode.iot.device.message.IDeviceDataService;
+import org.damocode.iot.network.http.server.vertx.HttpServerProvider;
+import org.damocode.iot.network.mqtt.client.MqttClientProvider;
+import org.damocode.iot.network.mqtt.server.vertx.VertxMqttServerProvider;
 import org.damocode.iot.network.tcp.server.TcpServerProvider;
 import org.damocode.iot.supports.server.DefaultSendToDeviceMessageHandler;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -16,6 +22,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @Author: zzg
@@ -29,6 +36,9 @@ public class EasyIotAutoConfiguration {
 
     @Resource
     private IDeviceOperatorService deviceOperatorService;
+
+    @Resource
+    private List<IDeviceDataService> deviceDataServices;
 
     @Bean
     @ConfigurationProperties(prefix = "vertx")
@@ -52,9 +62,14 @@ public class EasyIotAutoConfiguration {
         return sessionManager;
     }
 
+    @Bean
+    public DeviceMessageConnector deviceMessageConnector() {
+        return new DeviceMessageConnector(deviceDataServices);
+    }
+
     @Bean(initMethod = "startup")
-    public DefaultSendToDeviceMessageHandler defaultSendToDeviceMessageHandler(DeviceSessionManager sessionManager, MessageHandler messageHandler){
-        return new DefaultSendToDeviceMessageHandler(serverId,sessionManager,messageHandler,null);
+    public DefaultSendToDeviceMessageHandler defaultSendToDeviceMessageHandler(DeviceSessionManager sessionManager, MessageHandler messageHandler,DecodedClientMessageHandler clientMessageHandler){
+        return new DefaultSendToDeviceMessageHandler(serverId,sessionManager,messageHandler,clientMessageHandler);
     }
 
     @Bean
@@ -65,6 +80,21 @@ public class EasyIotAutoConfiguration {
     @Bean
     public TcpServerProvider tcpServerProvider(Vertx vertx) {
         return new TcpServerProvider(vertx);
+    }
+
+    @Bean
+    public MqttClientProvider mqttClientProvider(Vertx vertx) {
+        return new MqttClientProvider(vertx);
+    }
+
+    @Bean
+    public VertxMqttServerProvider mqttServerProvider(Vertx vertx) {
+        return new VertxMqttServerProvider(vertx);
+    }
+
+    @Bean
+    public HttpServerProvider httpServerProvider(Vertx vertx) {
+        return new HttpServerProvider(vertx);
     }
 
 }
